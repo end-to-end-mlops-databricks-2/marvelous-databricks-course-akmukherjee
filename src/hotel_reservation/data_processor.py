fbe10f2b6a09e57045707504d9d072d07bf68900
akmukherjee <akmukherjee@gmail.com>
2025-01-30 11:40:53 -0500

    Add files for structure

---
+++ src/hotel_reservation/data_processor.py
@@ -0,0 +1,66 @@
+import datetime
+
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp, to_utc_timestamp
+from sklearn.model_selection import train_test_split
+
+from hotel_reservation.config import ProjectConfig
+
+
+class DataProcessor:
+    def __init__(self, pandas_df: pd.DataFrame, config: ProjectConfig):
+        self.df = pandas_df  # Store the DataFrame as self.df
+        self.config = config  # Store the configuration
+
+    def preprocess(self):
+        """Preprocess the DataFrame stored in self.df"""
+        pass
+        # Handle missing values and convert data types as needed
+
+
+        # Handle numeric features
+
+
+        # Fill missing values with mean or default values
+
+
+        # Convert categorical features to the appropriate type
+
+
+        # Extract target and relevant features
+
+
+    def split_data(self, test_size=0.2, random_state=42):
+        """Split the DataFrame (self.df) into training and test sets."""
+        train_set, test_set = train_test_split(self.df, test_size=test_size, random_state=random_state)
+        return train_set, test_set
+
+    def save_to_catalog(self, train_set: pd.DataFrame, test_set: pd.DataFrame, spark: SparkSession):
+        """Save the train and test sets into Databricks tables."""
+
+        train_set_with_timestamp = spark.createDataFrame(train_set).withColumn(
+            "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC")
+        )
+
+        test_set_with_timestamp = spark.createDataFrame(test_set).withColumn(
+            "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC")
+        )
+
+        train_set_with_timestamp.write.mode("append").saveAsTable(
+            f"{self.config.catalog_name}.{self.config.schema_name}.train_set"
+        )
+
+        test_set_with_timestamp.write.mode("append").saveAsTable(
+            f"{self.config.catalog_name}.{self.config.schema_name}.test_set"
+        )
+
+        spark.sql(
+            f"ALTER TABLE {self.config.catalog_name}.{self.config.schema_name}.train_set "
+            "SET TBLPROPERTIES (delta.enableChangeDataFeed = true);"
+        )
+
+        spark.sql(
+            f"ALTER TABLE {self.config.catalog_name}.{self.config.schema_name}.test_set "
+            "SET TBLPROPERTIES (delta.enableChangeDataFeed = true);"
+        )
